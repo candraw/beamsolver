84bd49a6789844ebd95866479d8884c1b3660d9f
Jannik Birk <birk.jannik@gmail.com>
2019-07-11 17:16:15 +0200

    fix force directions

--- beamsolver.py
+++ beamsolver.py
@@ -7,7 +7,7 @@ def link(ba, bb, lx, ly, g):
     n0, Q0, lx0, ly0, g0 = ba
     n1, Q1, lx1, ly1, g1 = bb
 
-    return [n0 + lx0 + n1, Q0 + ly0 + g0 + Q1, lx0-lx, ly0-ly, g0-g]
+    return [-n0 + lx0 + n1, -Q0 + ly0 + g0 + Q1, lx0-lx, ly0-ly, g0-g]
 
 def end(ba):
     n0, Q0, lx0, ly0, g0 = ba
@@ -17,8 +17,8 @@ b0 = create_symbols(0)
 b1 = create_symbols(1)
 b2 = create_symbols(2)
 
-l1 = link(b0, b1, 1, 0, 0)
-l2 = link(b1,b2,1,0,0)
+l1 = link(b0, b1, 0, 1, 0)
+l2 = link(b1, b2, 0, 1, 0)
 e = end(b2)
 
 print(linsolve(l1+l2+e, b0 + b1 + b2))
